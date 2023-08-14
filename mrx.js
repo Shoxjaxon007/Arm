98b29e1e13a2b22a03843bf58767a6b1ea9d536d
Shoxjaxon007 <shakarovshoxjaxon666@gmail.com>
2023-08-14 17:15:31 +0500

    fix

---
+++ mrx.js
@@ -0,0 +1,15 @@
+let a = prompt("Svetaforni rangini kiriting")
+
+if(a === "qizil"){
+    alert("To'xtab Turing Iltimos")
+}
+else if(a === "yashil"){
+    alert("Harakatlanishingiz mumkin oka")
+}
+else if(a === "sariq"){
+    alert("Diqqat tayorlaning")
+}
+
+else{
+    alert("...")
+}
